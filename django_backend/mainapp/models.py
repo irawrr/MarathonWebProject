62f5318144c8e2a9ae8160fb2d40d5b2e6012312
Derlie Monne <derlie.monne@gmail.com>
2023-12-10 20:36:13 +1000

    add(backend): add countries and flags to database

--- django_backend/mainapp/models.py
+++ django_backend/mainapp/models.py
@@ -12,7 +12,7 @@ class Gender(models.Model):
 
 class Country(models.Model):
     name = models.CharField(max_length=80)
-    flag = models.ImageField(upload_to='country_flags')
+    flag = models.ImageField(upload_to='images/country_flags')
 
     def __str__(self) -> str:
         return self.name
